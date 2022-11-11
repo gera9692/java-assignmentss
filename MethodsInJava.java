eda15e71656c8539851ecd31ee56f924870c3adb
gera9692 <93513811+gera9692@users.noreply.github.com>
2022-11-11 16:07:47 +0300

    Update MethodsInJava.java

--- MethodsInJava.java
+++ MethodsInJava.java
@@ -1,18 +1,42 @@
-
-package methods.in.java;
+//this is a modifying
+package methods;
 import java.util.Scanner;
 
 /**
  *
  * @author Students
  */
-public class MethodsInJava {
+public class Methods {
 
     /**
-     * @param args the command line arguments
+     * @return    
      */
-    public static void main(String[] args) {
+    public int max(int x,int y) {
         // TODO code application logic here
-    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    System.out.print("enter x");
+     Scanner input= new Scanner(System.in);
+      x=input.nextInt();
+    System.out.print("enter y");
     
+      y=input.nextInt();
+
+                 if (x>y)
+                 {
+                     return x;
+                 }
+                 else {
+                      return y;
+                 }
+    }
 }
+                
+                        
+    
+
