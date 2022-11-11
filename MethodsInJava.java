2501a1cd836b2b788f0552aa5470c91cf23d1fca
gera9692 <93513811+gera9692@users.noreply.github.com>
2022-11-11 16:07:07 +0300

    Add files via upload

---
+++ MethodsInJava.java
@@ -0,0 +1,18 @@
+
+package methods.in.java;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Students
+ */
+public class MethodsInJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
